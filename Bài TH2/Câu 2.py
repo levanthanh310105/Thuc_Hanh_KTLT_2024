0d20d73eb8c44b9d72d29888a1cfb732f62970ad
levanthanh310105 <thanhle310105@gmail.com>
2024-11-27 10:30:01 +0700

    Câu 2_2.py

---
+++ Bài TH2/Câu 2.py
@@ -0,0 +1,9 @@
+print("Le Van Thanh")
+print("235752021610020")
+import math;
+xl=int(input("nhap x1: ")) yl=int(input("nhap yl:"))
+x2=int(input("nhap x2: ")) y2=int(input("nhap x2: "))
+d1=(x2-x1)*(x2-x1);
+d2=(y2-yl)*(y2-yl);
+res-math.sqrt(dl+d2)
+print("Distance between two points", res)
