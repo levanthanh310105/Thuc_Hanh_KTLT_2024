c39b1ea217e4c42c6caae19ce98f0e159e4c93e8
levanthanh310105 <thanhle310105@gmail.com>
2024-11-29 20:02:27 +0700

    Câu 2.11.py

---
+++ Bài TH2/Câu 11.py
@@ -0,0 +1,10 @@
+print("Le Van Thanh")
+print("MSSV: 235752021610020")
+l=[1, 'python',4,7]
+k=['cse',2,'guntur',8]
+m=[]
+m.append(l);
+m.append(k);
+print (m)
+d={1:1,2:k, 'combine list':m}
+print (d)
