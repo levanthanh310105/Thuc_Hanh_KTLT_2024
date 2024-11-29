a9ffdec6445625a13ce943d1fea9264274baf63b
levanthanh310105 <thanhle310105@gmail.com>
2024-11-29 19:51:41 +0700

    Câu 2.8.py

---
+++ Bài TH2/Câu 8.py
@@ -0,0 +1,11 @@
+print("Le Van Thanh")
+print("MSSV: 235752021610020")
+a, b 1, 2
+total = 0
+print(a,end="")
+while (a<=4000000-1):
+   if a %2==0:
+       total +=a
+   a, bb, a+b
+   print(a,end="")
+print("\n sum of prime numbers term in fiboncacci series:",total)
