9e007735eac56f03c379acf3529713e9bd0cb28c
levanthanh310105 <thanhle310105@gmail.com>
2024-11-30 20:17:19 +0700

    Câu  3.7.py

---
+++ Bài TH3/Câu 7.pyu 7.py
@@ -0,0 +1,8 @@
+print("Le Van Thanh")
+print("MSSV:235752021610020")
+def checkValue (n):
+ if n%2 == 0:
+  print ("This is an even number")
+ else:
+  print ("This is an odd number")
+checkValue (7)
