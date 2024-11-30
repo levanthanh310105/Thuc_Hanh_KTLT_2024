bcb80307c0bb2d97764796b7bf90f70ae8e8c092
levanthanh310105 <thanhle310105@gmail.com>
2024-11-30 22:20:08 +0700

    Câu 4.9.py

--- Bài TH4/Câu 9.py
+++ Bài TH4/Câu 9.py
@@ -1,7 +1,3 @@
 print("le van thanh")
 print("mssv: 235752021610020")
 ds = input ('Nhap chuoi: ').split()
-ds.remove('123')
-for ch in ds:
- print(" vị tri cua chuoi abc la", ds.index('abc'))
- print (ch)
