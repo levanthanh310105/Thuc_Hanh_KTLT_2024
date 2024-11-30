91350acf46d35230d9929b93c5774df8100a08f8
levanthanh310105 <thanhle310105@gmail.com>
2024-11-30 22:09:32 +0700

    Câu 4.5.py

---
+++ Bài TH4/Câu 5.py
@@ -0,0 +1,9 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+ds = input('Danh sach: ').split()
+# in cả dãy vừa nhập
+print(ds)
+# in day vua nhap, moi phan tu tren mot dong
+ds = ds [::-1]
+for so in ds:
+   print(so)
