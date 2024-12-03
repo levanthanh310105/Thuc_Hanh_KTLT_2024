6255331b66ea75a09abc86f1f1a7cad9c5462a1c
levanthanh310105 <thanhle310105@gmail.com>
2024-12-03 20:55:15 +0700

    Câu 5.6.py

---
+++ Bài TH5/Câu 6.py
@@ -0,0 +1,11 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+import numpy as np
+mang = np.arange(12, 38)
+mang_nguoc = mang[::-1]
+vi_tri_max = np.argmax(mang_nguoc)  
+vi_tri_min = np.argmin(mang_nguoc) 
+
+print("Mảng sau khi đảo ngược:", mang_nguoc)
+print("Vị trí phần tử lớn nhất:", vi_tri_max)
+print("Vị trí phần tử nhỏ nhất:", vi_tri_min)
