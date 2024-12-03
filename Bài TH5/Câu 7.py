d2d11e860f0f68a7d250a2ebe1b84aadc9ff6206
levanthanh310105 <thanhle310105@gmail.com>
2024-12-03 20:56:01 +0700

    Câu 5.7.py

---
+++ Bài TH5/Câu 7.py
@@ -0,0 +1,16 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+import numpy as np
+data_type = [('name', 'S15'), ('class', int), ('height', float)]
+students_details = [
+    ('James', 5, 48.5),
+    ('Nail', 6, 52.5),
+    ('Paul', 5, 42.10),
+    ('Pit', 5, 40.11)
+]
+students = np.array(students_details, dtype=data_type)
+print("Original array:")
+print(students)
+print("\nSort by height:")
+print(np.sort(students, order='height'))
+
