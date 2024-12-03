e08dad24805f9f8041d8aebe7cacde8c92b6abbc
levanthanh310105 <thanhle310105@gmail.com>
2024-12-03 20:45:49 +0700

    Câu 5.3.py

---
+++ Bài TH5/Câu 3.py
@@ -0,0 +1,10 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+
+import numpy as np
+
+# Tạo mảng với các giá trị từ 12 đến 37
+mang = np.arange(12, 38)
+
+# In kết quả
+print("Kết quả với các giá trị từ 12 đến 37 là:", mang)
