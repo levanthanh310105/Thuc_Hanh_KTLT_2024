0dd31d88dc9c67512b6e6482cd45fa89e991512a
levanthanh310105 <thanhle310105@gmail.com>
2024-12-03 20:47:22 +0700

    Câu 5.4.py

---
+++ Bài TH5/Câu 4.py
@@ -0,0 +1,14 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+
+import numpy as np
+
+# Tạo mảng với các giá trị từ 12 đến 37
+mang = np.arange(12, 38)
+
+# In kết quả ban đầu
+print("Mảng ban đầu:",# Đảo ngược mảng
+mang_nguoc = mang[::-1]
+
+# In kết quả sau khi đảo ngược
+print("Mảng sau khi đảo ngược:", mang_nguoc)
