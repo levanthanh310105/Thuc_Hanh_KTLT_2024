41fcb878602e852153be81fb127ede01493c200e
levanthanh310105 <thanhle310105@gmail.com>
2024-12-03 20:25:15 +0700

    Câu 5.1.py

---
+++ Bài TH5/Câu 1.py
@@ -0,0 +1,11 @@
+print("le van thanh")
+print("mssv: 235752021610020")
+import mymath
+
+a = 10
+b = 5
+
+print("Add:", mymath.add(a, b))
+print("Subtract:", mymath.subtract(a, b))
+print("Multiply:", mymath.multiply(a, b))
+print("Divide:", mymath.divide(a, b))
